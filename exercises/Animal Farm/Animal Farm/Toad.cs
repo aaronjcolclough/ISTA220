318168042ebdc2630a407b5d94c9f2831ed8a4ab
Aaron Colclough <aaron.j.colclough@outlook.com>
2018-09-04 09:08:42 -0400

    Sep 4 2018

---
+++ exercises/Animal Farm/Animal Farm/Toad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal_Farm
+{
+    class Toad
+    {
+        public string name = "Mortimer";
+
+        public string speak()
+        {
+            return "I'm basically useless.";
+        }
+
+        public string drink()
+        {
+            return "Nope, can't do that.";
+        }
+
+        public string fight()
+        {
+            return "Can't do that either.";
+        }
+
+        public string rest()
+        {
+            return "I suck.";
+        }
+    }
+}
--- exercises/playpen/playpen/Program.cs
+++ exercises/playpen/playpen/Program.cs
@@ -1,136 +1,32 @@
 ﻿using System;
 
-namespace playpen
+namespace Animal_Farm
 {
     class Program
     {
-        static void Main(string[] args)        
+        static void Main(string[] args)
         {
-            optionMenu();
+            //int start = 2;
+            Console.WriteLine("0, 1");
+            fibonacci(0, 1, 2);            
         }
 
-        private static void optionMenu()
+        private static void fibonacci(int v1, int v2, int start)
         {
-            Console.WriteLine("1- Sum of grades.");
-            Console.WriteLine("2- Assign letter to a grade.");
-            Console.WriteLine("3- Average 10 grades and assign a letter grade.");
-            Console.WriteLine("4- Average a specific number of grades and assign a letter grade.");
-            Console.WriteLine("5- Average an unknown number of grades and assign a letter grade.");
-            Console.WriteLine("Enter program to run:");
-            int menu = Convert.ToInt32(Console.ReadLine());
-            if (menu == 1)
-            {
-                double start = 0;
-                double end = 10;
-                double sum = 0;
-                get_sum(start, end, sum);
-            }
-            else if (menu == 2)
-            {
-                Console.WriteLine("Enter grade to be converted:");
-                double grade = Convert.ToDouble(Console.ReadLine());
-                char lettergrade = get_letter(grade);
-                Console.WriteLine($"The letter grade is {lettergrade}");
-            }
-            else if (menu == 3)
-            {
-                double start = 0;
-                double end = 10;
-                double sum = 0;
-                get_ave(start, end, sum);
-            }
-            else if (menu == 4)
-            {
-                Console.WriteLine("Enter number of grades to be averaged:");
-                double end = Convert.ToInt32(Console.ReadLine());
-                double start = 0;
-                double sum = 0;
-                get_ave(start, end, sum);
-            }
-            else if (menu == 5)
-            {
-                double start = 0;
-                double sum = 0;
-                get_sum(start, sum);
-            }
-        }
+            start++;
+            int end = 9;
+            int sum = v1 + v2;            
 
-        private static void get_sum(double start, double sum)
-        {
-            start = start + 1;            
-            Console.WriteLine($"Enter grade {start}:");
-            double g1 = Convert.ToDouble(Console.ReadLine());
-            sum = sum + g1;
-            if (g1 == -1)
-            {
-                Console.WriteLine($"The sum is {sum + 1}");
-                get_ave(sum + 1, start - 1);
-            }
-            else
-            {
-                get_sum(start, sum);
-            }
-        }
-
-        private static void get_ave(double start, double end, double sum)
-        {
-            start = start + 1;
-            Console.WriteLine($"Enter grade {start}:");
-            double g1 = Convert.ToDouble(Console.ReadLine());
-            sum = sum + g1;
             if (start < end)
             {
-                get_ave(start, end, sum);
+                Console.WriteLine($"{v2}, {sum}");
+                fibonacci(v2, sum, start);                                
             }
             else
             {
-                Console.WriteLine($"The sum is {sum}");
-                get_ave(sum, start);
+                Console.WriteLine($"{v2}, {sum}");
+                Console.WriteLine($"Number {start} in the Fibonacci Sequence is {sum}");
             }
         }
-
-        private static char get_letter(double grade)
-        {
-            if (grade >= 90) return 'A';
-            else if (grade >= 80) return 'B';
-            else if (grade >= 70) return 'C';
-            else if (grade >= 60) return 'D';
-            else return 'F';
-        }
-
-        private static void get_sum(double start, double end, double sum)
-        {
-            start = start + 1;
-            Console.WriteLine($"Enter grade {start}:");
-            double g1 = Convert.ToDouble(Console.ReadLine());
-            sum = sum + g1;
-            if  (start < end)
-            {
-                get_sum(start, end, sum);
-            }
-            else
-            {
-                Console.WriteLine($"The sum is {sum}");
-            }
-        }
-
-        private static void get_ave(double sum, double start)
-        {
-            double ave = sum / start;
-            Console.WriteLine($"The average is {ave}");
-            char lettergrade = getletter(ave);
-            Console.WriteLine($"The grade is {lettergrade}");
-        }
-
-        private static char getletter(double ave)
-        {
-            if (ave >= 90) return 'A';
-            else if (ave >= 80) return 'B';
-            else if (ave >= 70) return 'C';
-            else if (ave >= 60) return 'D';
-            else return 'F';
-        }
-
     }
-    
 }
